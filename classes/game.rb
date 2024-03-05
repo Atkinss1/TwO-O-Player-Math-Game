d8b7f6be6866696868296dee9198b5ef33064986
Atkinss <jordanatkins@live.com>
2024-03-04 21:50:33 -0700

    add: logic to addition case

--- classes/game.rb
+++ classes/game.rb
@@ -1,15 +1,28 @@
+require_relative 'question'
+require_relative 'player'
 
 class Game
   include Question
 
   def initialize
+    @player = Player.new('Player 1')
     @answer = 0
   end
 
   def question(number)
     case number
     when 1
-      addition
+      question, @answer = addition
+      puts question
+      input = gets.chomp.to_i
+        if integer(input) 
+          if @answer == input
+            puts "#{input}"
+            @player.increment_score
+            puts "score updated!"
+            puts "Player 1 score is: #{@player.score}"
+            end
+        end 
     when 2
       subraction
     when 3
@@ -20,5 +33,15 @@ class Game
       "Enter a number between 1 & 4."
     end
   end
+
+  def integer(input)
+    if input.to_s.match?(/\A\d+\z/)
+      return true
+    else
+      puts "Input a number."
+      false
+    end
+  end
+
 end
 
