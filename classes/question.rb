9a15b7218c9ed4d7543093e232e2f711aeb1af1d
Atkinss <jordanatkins@live.com>
2024-03-04 21:51:07 -0700

    add: logic to math equations

--- classes/question.rb
+++ classes/question.rb
@@ -1,29 +1,30 @@
 module Question
-  
+
   private
 
   def addition
-    integer_one = rand(100)
-    integer_two = rand(100)
-    "#{integer_one} + #{integer_two}"
+    addition = [rand(100), rand(100)]
+    question = "What is: #{addition[0]} + #{addition[1]}"
+    result = addition.reduce(:+)
+    [question, result]
   end
 
   def subraction
     integer_one = rand(100)
     integer_two = rand(100)
-    "#{integer_one} - #{integer_two}"
+    "What is: #{integer_one} - #{integer_two}"
   end
 
   def multiplication
     integer_one = rand(10)
     integer_two = rand(10)
-    "#{integer_one} * #{integer_two}"
+    "What is: #{integer_one} * #{integer_two}"
   end
 
   def division
     integer_one = rand(10)
     integer_two = rand(10)
-    "#{integer_one} / #{integer_two}"
+    "What is: #{integer_one} / #{integer_two}"
   end
 
 end
