64159e22615151e81792459c6af0a2125ff22cf5
Atkinss <jordanatkins@live.com>
2024-03-05 19:10:41 -0700

    refactor logic

--- classes/game.rb
+++ classes/game.rb
@@ -8,7 +8,7 @@ class Game
     @player_one = Player.new(player1)
     @player_two = Player.new(player2)
     @current_player = @player_one
-    @question_number = 1
+    @question_number = 18
     @answer = 0
   end
 
@@ -16,113 +16,71 @@ class Game
     puts "--- Welcome to the Math Game --- "
     puts "      --- #{@player_one} & #{@player_two} ---\n\n"
 
-    loop do
-      question(rand(4))
-      break if @player_one.life == 0 || @player_two.life == 0 || @question_number > 20
+    20.times do |index|
+      break if game_over?
+
+      ask_question
+      next_players_turn
     end
 
-    winner = @player_one.score > @player_two.score ? @player_one : @player_two
-
-    puts "Congrats #{winner}! You have the higher score!"
+    winner
   end
 
-  def question(number)
-    case number
-    when 1
-      question, @answer = addition
+
+  def ask_question
+    question, @answer = generate_question
       puts "Question # #{@question_number}: "
       puts "#{@current_player} #{question}"
-      input = gets.chomp.to_i
-        if integer?(input) 
-          if @answer == input
-            puts "YES! You are correct."
-            @current_player.increment_score
-            puts "#{@current_player}'s score is: #{@current_player.score}\n\n"
-            @question_number += 1
-            next_players_turn
-          else
-            puts "Seriously? No!"
-            @current_player.minus_life
-            puts "--- #{@player_one}: #{@player_one.life} / 3 vs #{@player_two}: #{@player_two.life} / 3 ---"
-            @question_number += 1
-            next_players_turn
-          end
-        end 
-    when 2
-      question, @answer = subtraction
-      puts "Question # #{@question_number}: "
-      puts "#{@current_player} #{question}"
-      input = gets.chomp.to_i
-        if integer?(input) 
-          if @answer == input
-            puts "YES! You are correct."
-            @current_player.increment_score
-            puts "#{@current_player}'s score is: #{@current_player.score}\n\n"
-            @question_number += 1
-            next_players_turn
-          else
-            puts "Seriously? No!"
-            @current_player.minus_life
-            puts "--- #{@player_one}: #{@player_one.life} / 3 vs #{@player_two}: #{@player_two.life} / 3 ---"
-            @question_number += 1
-            next_players_turn
-          end
-        end 
-    when 3
-      question, @answer = multiplication
-      puts "Question # #{@question_number}: "
-      puts "#{@current_player} #{question}"
-      input = gets.chomp.to_i
-        if integer?(input) 
-          if @answer == input
-            puts "YES! You are correct."
-            @current_player.increment_score
-            puts "#{@current_player}'s score is: #{@current_player.score}\n\n"
-            @question_number += 1
-            next_players_turn
-          else
-            puts "Seriously? No!"
-            @current_player.minus_life
-            puts "--- #{@player_one}: #{@player_one.life} / 3 vs #{@player_two}: #{@player_two.life} / 3 ---"
-            @question_number += 1
-            next_players_turn
-          end
-        end 
-    when 4
-      question, @answer = division
-      puts "Question # #{@question_number}: "
-      puts "#{@current_player} #{question}"
-      input = gets.chomp.to_i
-        if integer?(input) 
-          if @answer == input
-            puts "YES! You are correct."
-            @current_player.increment_score
-            puts "#{@current_player}'s score is: #{@current_player.score}\n\n"
-            @question_number += 1
-            next_players_turn
-          else
-            puts "Seriously? No!"
-            @current_player.minus_life
-            puts "--- #{@player_one}: #{@player_one.life} / 3 vs #{@player_two}: #{@player_two.life} / 3 ---"
-            @question_number += 1
-            next_players_turn
-          end
-        end 
+      input = get_integer_input
+      process_answer(input)
+  end
+
+  def generate_question
+    case rand(1..4)
+    when 1 then addition
+    when 2 then subtraction
+    when 3 then multiplication
+    when 4 then division
     end
+  end
+
+  def get_integer_input
+    print "Your answer is: "
+    input = gets.chomp.to_i
+    return input if input.to_s.match?(/\A-?\d+\z/)
+    puts "Invalid input. Please enter a number."
+    get_integer_input
+  end
+
+  def process_answer(input)
+    if @answer == input
+      puts "YES! You are correct."
+      @current_player.increment_score
+      
+    else
+      puts "Seriously? No!"
+      puts "The correct answer was #{@answer}.."
+      @current_player.minus_life
+      puts "--- #{@player_one}: #{@player_one.life} / 3 vs #{@player_two}: #{@player_two.life} / 3 ---"
+
+    end
+    puts "#{@current_player}'s score is: #{@current_player.score}\n\n"
+      @question_number += 1
+      next_players_turn
   end
 
   def next_players_turn
     @current_player = (@current_player == @player_one) ? @player_two : @player_one
-    question(rand(4))
   end
-  
-  def integer?(input)
-    if input.to_s.match?(/\A-?\d+\z/)
-      return true
-    else
-      puts "Input a number."
-      false
-    end
+
+  def game_over?
+    @player_one.life.zero? || @player_two.life.zero? || @question_number > 20
+  end
+
+  def winner
+    first_place = (@player_one.score > @player_two.score) ? @player_one : @player_two
+
+    puts "Congrats #{first_place}! You have the higher score!"
   end
 
 end
