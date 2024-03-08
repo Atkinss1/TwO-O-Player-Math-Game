a28d4b2148c015ab920e5b369ec93e9b5f3fc8ad
Atkinss <jordanatkins@live.com>
2024-03-07 23:02:09 -0700

    add: player input for names

--- main.rb
+++ main.rb
@@ -1,6 +1,11 @@
 require './classes/game.rb'
 
+puts "Welcome! Please enter Player 1s name: "
+player1 = gets.chomp.to_s
+puts "Please enter Player 2s name: "
+player2 = gets.chomp.to_s
 
-new_game = Game.new("John", "Jim")
+puts "GOOD LUCK!"
+new_game = Game.new("#{player1}", "#{player2}")
 
 puts new_game.start
